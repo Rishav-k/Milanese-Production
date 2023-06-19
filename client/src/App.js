1986df7cea638311e20b1e663a1adb5f2ef364fa
Rishav Kumar <rishav_k@ce.iitr.ac.in>
2023-06-20 00:40:47 +0530

    first commit11

--- client/src/App.js
+++ client/src/App.js
@@ -29,7 +29,7 @@ function App() {
       <Route path="/products/">
            <Route index path=":id" element={<Customiser />} />
            <Route path=":id/address" element = {<Address/>} />
-           {/* <Route path=":id/address/checkout" element={<Checkout />} /> */}
+           <Route path=":id/address/checkout" element={<Checkout />} />
       </Route>
 
      {/* <Route path="/products/:id" element={<Customiser />}></Route> */}
