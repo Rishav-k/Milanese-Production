59357acf3b2bc566438d0ad8b26062bddee5fe5d
Rishav Kumar <rishav_k@ce.iitr.ac.in>
2023-06-20 00:57:18 +0530

    first commit15

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Customiser from './components/customiser/Customise.js'
 import SignIn from './components/signin/SignIn.js';
 import Address from './components/orders/Address.js';
-import Checkout from './components/orders/Checkout.js';
+// import Checkout from './components/orders/Checkout.js';
 import FormContext from './components/context/FormContext.js';
 import useForm from './components/context/useForm.js';
 import Test from './components/orders/Test.js';
@@ -25,7 +25,7 @@ function App() {
      <Routes>
      <Route path= "test" element={<Test/>} />
      <Route path="signin" element = {<SignIn/>} />
-     <Route path="checkout" element={<Checkout />} />
+     {/* <Route path="checkout" element={<Checkout />} /> */}
       <Route path="/products/">
            <Route index path=":id" element={<Customiser />} />
            <Route path=":id/address" element = {<Address/>} />
